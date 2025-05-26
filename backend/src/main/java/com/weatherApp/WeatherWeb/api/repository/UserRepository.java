1163f7e6d60b440f125401410631a4ebfd5cbcec
benjaminpeiterDHBW <peiter.benjami-it24@it.dhbw-ravensburg.de>
2025-05-26 07:11:58 +0200

    fix spelling error

--- backend/src/main/java/com/weatherApp/WeatherWeb/api/repository/UserRepository.java
+++ backend/src/main/java/com/weatherApp/WeatherWeb/api/repository/UserRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.email = ?1")
-    public User findByEmail(String email);
+import com.weatherApp.WeatherWeb.api.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
